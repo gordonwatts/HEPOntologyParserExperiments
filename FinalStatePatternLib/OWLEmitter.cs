844e0d49966c84c027e3af6afc72cab5c1da4dcb
Gordon Watts <gordon@gordonandpaula.net>
2015-07-25 03:32:17 +0200

    Added first OWL emitter

---
+++ FinalStatePatternLib/OWLEmitter.cs
@@ -0,0 +1,28 @@
+﻿using FinalStatePatternLib.OWLData;
+using System.IO;
+
+namespace FinalStatePatternLib
+{
+    public static class OWLEmitter
+    {
+        /// <summary>
+        /// Emit a detector final state
+        /// </summary>
+        /// <param name="dfs"></param>
+        /// <param name="wr"></param>
+        public static void Emit(this DetectorFinalState dfs, TextWriter wr)
+        {
+
+        }
+
+        /// <summary>
+        /// Emit a fso
+        /// </summary>
+        /// <param name="fso"></param>
+        /// <param name="wr"></param>
+        public static void Emit(this FinalStateObject fso, TextWriter wr)
+        {
+            wr.WriteLine("<#{0}> rdf:type dfs:PhysicsObject {1}", fso.Name, fso.BaseDefinition == null ? "." : fso.BaseDefinition);
+        }
+    }
+}
---
+++ t_FinalStatePatternLib/t_OWLEmitter.cs
@@ -0,0 +1,31 @@
+﻿using FinalStatePatternLib;
+using FinalStatePatternLib.OWLData;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace t_FinalStatePatternLib
+{
+    [TestClass]
+    public class t_OWLEmitter
+    {
+        [TestMethod]
+        public void EmitAFSOWithNoBaseDefinition()
+        {
+            var fso = new FinalStateObject() { Name = "hi", BaseDefinition = null };
+            var ms = new StringWriter();
+            fso.Emit(ms);
+            var text = ms.ToString().Trim();
+            Assert.AreEqual("<#hi> rdf:type dfs:PhysicsObject .", text);
+        }
+
+        [TestMethod]
+        public void EmitAFSOWithBaseDefinition()
+        {
+            var fso = new FinalStateObject() { Name = "hi", BaseDefinition = "fork" };
+            var ms = new StringWriter();
+            fso.Emit(ms);
+            var text = ms.ToString().Trim();
+            Assert.AreEqual("<#hi> rdf:type dfs:PhysicsObject; \n  hasBaseDefinition: fork .", text);
+        }
+    }
+}
