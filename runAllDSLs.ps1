fe5fcb7533934df6b918ed5f36fdf960a92eb3ed
Gordon Watts <gordon@gordonandpaula.net>
2016-01-05 21:22:42 -0800

    Fix up debug vs release builds

--- runAllDSLs.ps1
+++ runAllDSLs.ps1
@@ -1,4 +1,4 @@
 # Run on all the DSL files in the PaperDSLs directory, produce output owl files.
 # We use this in the build CI. That way we can avoid having to escape the '"'.
 
-ls .\PaperDSLs\*.txt | % {.\ParseSingleDFS\bin\Debug\ParseSingleDFS.exe $_ > $_.Name.Replace(".txt", ".owl")}
+ls .\PaperDSLs\*.txt | % {.\ParseSingleDFS\bin\Release\ParseSingleDFS.exe $_ > $_.Name.Replace(".txt", ".owl")}
